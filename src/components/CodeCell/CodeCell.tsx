0d154b2cbfbe02a4a2e7b31e4d02e12685248941
Sandro Kakashvili <sandrokakashvili@gmail.com>
2021-08-02 22:26:17 +0400

    feat: create bundle

--- src/components/CodeCell/CodeCell.tsx
+++ src/components/CodeCell/CodeCell.tsx
@@ -1,31 +1,34 @@
-import { useState, useEffect } from "react";
-import bundler from "../../bundler";
+import { useEffect } from "react";
 import CodeEditor from "../CodeEditor/CodeEditor";
 import Preview from "../Preview/Preview";
 import Resizable from "../Resizable/Resizable";
 import { Cell } from "../../state";
 import { useActions } from "../../hooks/use-actions";
+import { useTypedSelector } from "../../hooks/use-typed-selector";
 
 interface CodeCellProps {
   cell: Cell;
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
-  const [code, setCode] = useState<string>("");
-  const [error, setError] = useState<string>("");
-  const { updateCell } = useActions();
+  const { updateCell, createBundle } = useActions();
+  const bundle = useTypedSelector(({ bundles }) => bundles?.[cell.id]);
 
   useEffect(() => {
-    const timer = setTimeout(async () => {
-      const { code, err } = await bundler(cell.content);
-      setCode(code);
-      setError(err);
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      createBundle(cell.id, cell.content);
     }, 750);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [cell.content]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cell.id, cell.content, createBundle]);
 
   return (
     <Resizable direction="vertical">
@@ -42,7 +45,7 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        <Preview code={code} error={error} />
+        {bundle && <Preview code={bundle.code} error={bundle.err} />}
       </div>
     </Resizable>
   );
