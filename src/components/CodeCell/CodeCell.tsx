693c85969ace7e0d6f085ff0b0cc434a5caf165c
Sandro Kakashvili <sandrokakashvili@gmail.com>
2021-08-02 23:15:56 +0400

    feat: show function

--- src/components/CodeCell/CodeCell.tsx
+++ src/components/CodeCell/CodeCell.tsx
@@ -14,16 +14,62 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
   const bundle = useTypedSelector(({ bundles }) => bundles?.[cell.id]);
+  const cumulativeCode: string[] = useTypedSelector((state) => {
+    if (state.cells) {
+      const { data, order } = state.cells;
+      const orderedCells = order.map((id) => data[id]);
+
+      const showFunc = `
+        import _React from 'react';
+        import _ReactDOM from 'react-dom';
+        var show = (value) => {
+          const root = document.querySelector('#root');
+          if (typeof value === 'object') {
+            if (value.$$typeof && value.props) {
+              _ReactDOM.render(value, root);
+            } else {
+              root.innerHTML = JSON.stringify(value);
+            }
+          } else {
+            root.innerHTML = value;
+          }
+        };
+      `;
+      const showFuncNoop = `var show = () => {}`;
+      const cumulativeCode: string[] = [];
+      for (const orderedCell of orderedCells) {
+        if (orderedCell.type === "code") {
+          if (orderedCell.id === cell.id) {
+            cumulativeCode.push(showFunc);
+          } else {
+            cumulativeCode.push(showFuncNoop);
+          }
+          cumulativeCode.push(orderedCell.content);
+        }
+        if (orderedCell.id === cell.id) {
+          break;
+        }
+      }
+      return cumulativeCode;
+    }
+    return [""];
+  });
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cumulativeCode.join("\n"));
+      return;
+    }
+
     const timer = setTimeout(() => {
-      createBundle(cell.id, cell.content);
+      createBundle(cell.id, cumulativeCode.join("\n"));
     }, 750);
 
     return () => {
       clearTimeout(timer);
     };
-  }, [cell.id, cell.content, createBundle]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cell.id, cumulativeCode.join("\n"), createBundle]);
 
   return (
     <Resizable direction="vertical">
